0f52934584d5080b9465ea200d61e87e4ce3713c
MatiasSaibene <matiassaibene98@gmail.com>
2025-01-02 21:42:02 -0300

    4th Rewrite and folder structure changed

---
+++ src_Windows/main.cpp
@@ -0,0 +1,545 @@
+/////////////////////////////////////////////////////
+//Copyright (c)2024 Matías Saibene                 //
+//ORBITER MODULE: UNIVERSAL CARS FOR ORBITER (UCFO)//
+//      Licenced under the MIT Licence             //
+// main.cpp  Main implementation file              //
+/////////////////////////////////////////////////////
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#define ORBITER_MODULE
+#include "main.h"
+
+//Constructor
+UCFO::UCFO(OBJHANDLE hVessel, int flightmodel) : 
+VESSEL4(hVessel, flightmodel){
+
+	mhUCFO = NULL;
+
+	wheel_base = front_right_wheel_contact.z - rear_right_wheel_contact.x;
+
+    wheel_track = front_right_wheel_contact.x - front_left_wheel_contact.x;
+
+	front_stiffness = 0.0;
+
+	front_damping = 0.0;
+
+	rear_stiffness = 0.0;
+
+	rear_damping = 0.0;
+
+	body_stiffness = 0.0;
+
+	body_damping = 0.0;
+
+	drive_status = 'F';
+
+	empty_mass = 0.0;
+
+	MeshName[0] = '\0';
+
+	size = 0.0;
+	
+
+}
+
+//Destructor
+UCFO::~UCFO(){
+
+}
+
+void UCFO::clbkSetClassCaps(FILEHANDLE cfg){
+
+
+	//Get vessel parameters from configuration file,
+    //and set the physical vessel parameters.
+
+	if(!oapiReadItem_string(cfg, "Mesh", MeshName)){
+		TerminateAtError("Mesh", GetName(), "car");
+	}
+
+	SetMeshVisibilityMode (AddMesh (mhUCFO = oapiLoadMeshGlobal (MeshName)), MESHVIS_EXTERNAL);
+
+
+	if(!oapiReadItem_float(cfg, "Size", size)){
+		TerminateAtError("Size", GetName(), "car");
+	}
+
+	SetSize(size);
+
+
+	if(!oapiReadItem_float(cfg, "Mass", empty_mass)){
+		TerminateAtError("Mass", GetName(), "car");
+	}
+
+	SetEmptyMass(empty_mass);
+
+
+
+
+	if(!oapiReadItem_int(cfg, "FrontLeftWheelID", front_left_wheel_id)){
+		TerminateAtError("FrontLeftWheelID", GetName(), "car");
+	}
+
+	oapiWriteLogV("%s: FrontLeftWheelID: %d", GetName(), front_left_wheel_id);
+
+
+	if(!oapiReadItem_int(cfg, "FrontRightWheelID", front_right_wheel_id)){
+		TerminateAtError("FrontRightWheelID", GetName(), "car");
+	}
+
+	oapiWriteLogV("%s: FrontRightWheelID: %d", GetName(), front_right_wheel_id);
+
+
+	if(!oapiReadItem_int(cfg, "RearRightWheelID", rear_right_wheel_id)){
+		TerminateAtError("RearRightWheelID", GetName(), "car");
+	}
+
+	oapiWriteLogV("%s: RearRightWheelID: %d", GetName(), rear_right_wheel_id);
+
+
+	if(!oapiReadItem_int(cfg, "RearLeftWheelID", rear_left_wheel_id)){
+		TerminateAtError("RearLeftWheelID", GetName(), "car");
+	}
+
+	oapiWriteLogV("%s: RearLeftWheelID: %d", GetName(), rear_left_wheel_id);
+
+
+	if(!oapiReadItem_vec(cfg, "FrontLeftWheelPosition", front_left_wheel_pos)){
+		TerminateAtError("FrontLeftWheelPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "FrontRightWheelPosition", front_right_wheel_pos)){
+		TerminateAtError("FrontRightWheelPosition", GetName(), "car");
+	}
+	
+	if(!oapiReadItem_vec(cfg, "RearLeftWheelPosition", rear_left_wheel_pos)){
+		TerminateAtError("RearLeftWheelPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RearRightWheelPosition", rear_right_wheel_pos)){
+		TerminateAtError("RearRightWheelPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "LeftHeadlightPosition", left_headlight_pos)){
+		TerminateAtError("LeftHeadlightPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RightHeadlightPosition", right_headlight_pos)){
+		TerminateAtError("RightHeadlightPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "LeftTailLightPosition", left_tail_light_pos)){
+		TerminateAtError("LeftTailLightPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RightTailLightPosition", right_tail_light_pos)){
+		TerminateAtError("RightTailLightPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "LeftBackupLightPosition", left_backup_pos)){
+		TerminateAtError("LeftBackupLightPosition", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RightBackupLightPosition", right_backup_pos)){
+		TerminateAtError("RightBackupLightPosition", GetName(), "car");
+	}
+
+
+
+	if(!oapiReadItem_vec(cfg, "CameraPosition", camera_pos)){
+		TerminateAtError("CameraPosition", GetName(), "car");
+	}
+
+	SetCameraOffset(camera_pos);
+
+
+
+	if(!oapiReadItem_vec(cfg, "FrontRightWheelAxle", front_right_wheel_axle)){
+		TerminateAtError("FrontRightWheelAxle", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "FrontLeftWheelAxle", front_left_wheel_axle)){
+		TerminateAtError("FrontLeftWheelAxle", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RearRightWheelAxle", rear_right_wheel_axle)){
+		TerminateAtError("RearRightWheelAxle", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RearLeftWheelAxle", rear_left_wheel_axle)){
+		TerminateAtError("RearLeftWheelAxle", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "FrontRightWheelContact", front_right_wheel_contact)){
+		TerminateAtError("FrontRightWheelContact", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "FrontLeftWheelContact", front_left_wheel_contact)){
+		TerminateAtError("FrontLeftWheelContact", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RearRightWheelContact", rear_right_wheel_contact)){
+		TerminateAtError("RearRightWheelContact", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "RearLeftWheelContact", rear_left_wheel_contact)){
+		TerminateAtError("RearLeftWheelContact", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint1", TDP1)){
+		TerminateAtError("TouchdownPoint1", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint2", TDP2)){
+		TerminateAtError("TouchdownPoint2", GetName(), "car");
+	}
+	
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint3", TDP3)){
+		TerminateAtError("TouchdownPoint3", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint4", TDP4)){
+		TerminateAtError("TouchdownPoint4", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint5", TDP5)){
+		TerminateAtError("TouchdownPoint5", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint6", TDP6)){
+		TerminateAtError("TouchdownPoint6", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint7", TDP7)){
+		TerminateAtError("TouchdownPoint7", GetName(), "car");
+	}
+
+	if(!oapiReadItem_vec(cfg, "TouchdownPoint8", TDP8)){
+		TerminateAtError("TouchdownPoint8", GetName(), "car");
+	}
+
+
+
+	Fuel = CreatePropellantResource(UCFO_FUEL_MASS);
+
+
+
+	//Dummy thruster to provide throttle input to engine model. 1 N thrust is to allow thruster status
+    //to indicate throttle position
+
+	th_dummy = CreateThruster(ENGINE_LOCATION, FORWARD_DIRECTION, 1, Fuel, UCFO_ISP);
+
+	thg_dummy = CreateThrusterGroup(&th_dummy, 1, THGROUP_MAIN);
+
+	SetCW(0.5, 0.5, 0.3, 0.3);
+
+
+
+	static PARTICLESTREAMSPEC wheel_trails = {
+        0, 0.5, 5, 10, 0.03, 5, 1, 3.0, 
+        PARTICLESTREAMSPEC::EMISSIVE,
+		PARTICLESTREAMSPEC::LVL_PLIN, -1.0, 25.0,
+		PARTICLESTREAMSPEC::ATM_PLIN, 
+    };
+
+	AddParticleStream(&wheel_trails, front_left_wheel_pos, BACKWARD_DIRECTION, &lvlwheeltrails);
+
+    AddParticleStream(&wheel_trails, front_right_wheel_pos, BACKWARD_DIRECTION, &lvlwheeltrails);
+
+    AddParticleStream(&wheel_trails, rear_left_wheel_pos, BACKWARD_DIRECTION, &lvlwheeltrails);
+
+    AddParticleStream(&wheel_trails, rear_right_wheel_pos, BACKWARD_DIRECTION, &lvlwheeltrails);
+
+
+
+}
+
+void UCFO::clbkPostCreation(){
+
+	//Following is needed to allow the suspension to be set and tuned to the correct height independent of the planetary body it is on.
+
+	GetWeightVector(max_weight_vector);
+    max_weight = length(max_weight_vector);
+
+    max_weight_front = max_weight * (-rear_right_wheel_contact.z / wheel_base); //Weight supported by two front wheells.
+    max_weight_rear = max_weight - max_weight_front; //Weight supported by two rear wheels.
+
+    SetContactTouchdownPoints();
+
+}
+
+void UCFO::clbkPreStep(double simt, double simdt, double mjd){
+
+	//Determine strut travel for animations and to calculate normal contact forces on all wheels
+	pitch = GetPitch();
+	yaw = GetYaw();
+	bank = GetBank();
+	GetGroundspeedVector(FRAME_LOCAL, vel);
+	GetAngularVel(angvel);
+	omega = {x = 0, angvel.y, z = 0};
+	mass = empty_mass + GetPropellantMass(Fuel);
+
+	EnginePower();
+	Brakes();
+
+}
+
+void UCFO::EnginePower(){
+
+	//VW Thing engine specs
+	double displacement = 1.584e-3; //Engine displacement per cycle in cubic meters
+    int n_rev = 2; //Shaft revolutions per cycle (1 for 2-stroke, 2 for 4-stroke)
+    double r = 7.5; //Compression ratio
+    int idle_rpm = 700;
+    int max_rpm = 5000;
+
+	//Ambient air properties
+
+    double air_density = GetAtmDensity();
+    double air_temp = GetAtmTemperature();
+    double k = 1.113; //Polytropic specific heat ratio
+    int cp_air = 1005; //Specific heat of air J/kg K
+
+    //Fuel properties
+    double HV = 45e+6; //Lower heating value of gasoline J/kg
+    double AF = 14.7; //Stochiometric fuel air ratio for gasoline
+
+    double l_throttle = GetThrusterLevel(th_dummy);
+
+    double l_rpm = idle_rpm + l_throttle * (max_rpm - idle_rpm);
+
+    double l_omega = l_rpm * (2 * PI) / 60;
+
+    double l_mass_flow_air = air_density * displacement * (l_rpm / n_rev) * (1.0 / 60); //Air flow in kg/s
+    double l_mass_flow_gas= l_mass_flow_air / AF;
+
+    double l_QH = l_mass_flow_gas * HV;
+
+    double l_T1 = air_temp;
+    double l_T2 = l_T1 * (pow((r),(k-1)));
+    double l_T3 = l_T2 + ((l_QH) / (l_mass_flow_air * cp_air));
+    double l_T4 = l_T3 * (pow((1/r), (k-1)));
+
+    double l_QL = l_mass_flow_air * cp_air * (l_T4-l_T1);
+
+    double l_power = l_QH - l_QL;
+    double l_torque = l_power / l_omega;
+
+    double gph = 0.26 * l_mass_flow_gas * 3600;
+
+    VECTOR3 omega_wheel_aux = _V(0, 0, 0);
+    GetGroundspeedVector(FRAME_LOCAL, omega_wheel_aux);
+
+    double l_omega_wheel = omega_wheel_aux.z / wheel_radius;
+
+    double force = 0.0;
+
+    if(l_omega_wheel <= 0){
+        
+        force = l_throttle * mu_dyn * max_weight_rear;
+
+    } else if(l_omega_wheel > 0){
+
+        force = std::min((l_torque / wheel_radius) * (l_omega / l_omega_wheel), l_throttle * mu_dyn * max_weight_rear);
+
+    }
+
+    if(GroundContact()){
+
+        if(drive_status == 'F'){
+
+            AddForce(_V(0, 0, 0.5 * force), rear_right_wheel_contact);
+
+            AddForce(_V(0, 0, 0.5 * force), rear_left_wheel_contact);
+
+        } else if(drive_status == 'R'){
+
+            AddForce(_V(0, 0, -0.5 * force), rear_right_wheel_contact);
+
+            AddForce(_V(0, 0, -0.5 * force), rear_left_wheel_contact);
+
+        }
+
+    }
+
+    SetPropellantMass(Fuel, GetPropellantMass(Fuel) - l_mass_flow_gas * oapiGetSimStep());
+
+}
+
+void UCFO::Brakes(){
+
+	if(brake_status == 'N'){
+
+        if(length(vel) > 0){
+            AddForce((_V(0, 0, (-vel.z / length(vel)) * mu_dyn * max_weight)), _V(0, -wheel_radius, 0));
+        }
+
+        SetThrusterGroupLevel(THGROUP_MAIN, 0);
+
+    }
+
+}
+
+void UCFO::SetContactTouchdownPoints(){
+
+	front_stiffness = 0.5 * max_weight_front / travel;
+	front_damping = std::sqrt(4 * front_stiffness * empty_mass);
+
+    rear_stiffness = 0.5 * max_weight_rear / travel;
+    rear_damping = std::sqrt(4 * rear_stiffness * empty_mass);
+
+    body_stiffness = max_weight / travel;
+    body_damping = std::sqrt(4.0 * body_stiffness * empty_mass);
+
+    static TOUCHDOWNVTX td_points[ntdvtx]{
+        {(front_left_wheel_contact), front_stiffness, front_damping, 0.0, 0.0},
+        {(front_right_wheel_contact), front_stiffness, front_damping, 0.0, 0.0},
+        {(rear_left_wheel_contact), rear_stiffness, rear_damping, 0.0, 0.0},
+        {(rear_right_wheel_contact), rear_stiffness, rear_damping, 0.0, 0.0},
+        {(TDP1), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP2), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP3), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP4), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP5), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP6), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP7), body_stiffness, body_damping, 1.0, 1.0},
+        {(TDP8), body_stiffness, body_damping, 1.0, 1.0},
+    };
+
+    SetTouchdownPoints(td_points, ntdvtx);
+
+}
+
+double UCFO::UpdateLvlWheelsTrails(){
+
+    double speed = GetGroundspeed();
+
+
+    if((speed > 10.0) && (GroundContact())){
+
+        return 1.0;
+
+    } else {
+
+        return 0.0;
+
+    }
+
+}
+
+int UCFO::clbkConsumeBufferedKey(DWORD key, bool down, char *kstate){
+
+    if(key == OAPI_KEY_PERIOD && down){
+        
+        if(drive_status == 'F'){
+            
+            drive_status = 'N';
+
+        } else if(drive_status == 'N'){
+
+            drive_status = 'R';
+
+        } else if(drive_status == 'R'){
+
+            drive_status = 'R';
+        }
+
+    }
+
+    if(key == OAPI_KEY_COMMA && down){
+
+        if(drive_status == 'R'){
+
+            drive_status = 'N';
+
+        } else if(drive_status == 'N'){
+
+            drive_status = 'F';
+
+        } else if(drive_status == 'F'){
+
+            drive_status = 'F';
+
+        }
+
+    }
+
+
+    return 0;
+}
+
+int UCFO::clbkConsumeDirectKey(char *kstate){
+
+    if(KEYDOWN(kstate, OAPI_KEY_SPACE)){
+        
+        brake_status = 'N';
+
+    } else {
+        brake_status = 'F';
+    }
+
+    if(KEYDOWN(kstate, OAPI_KEY_NUMPAD1)){
+
+        steering_angle = std::max(steering_angle - 0.05 * RAD, -1.0);
+
+    }
+
+    if(KEYDOWN(kstate, OAPI_KEY_NUMPAD3)){
+        
+        steering_angle = std::min(steering_angle + 0.05 * RAD, 1.0);
+        
+    }
+
+    return 0;
+}
+
+void UCFO::clbkLoadStateEx(FILEHANDLE scn, void *vs){
+
+    char *line;
+
+    while(oapiReadScenario_nextline(scn, line)){
+        ParseScenarioLineEx(line, vs);
+    }
+
+}
+
+void UCFO::clbkPostStep(double simt, double simdt, double mjd){
+
+	lvlwheeltrails = UpdateLvlWheelsTrails();
+
+}
+
+void UCFO::TerminateAtError(const char *error, const char * className, const char *type){
+
+	oapiWriteLogV("ERROR: UCFO cannot continue: The %s of %s %s is not specified", error, className, type);
+
+    std::terminate();
+}
+
+DLLCLBK void InitModule(HINSTANCE hModule){
+
+}
+
+DLLCLBK void ExitModule(HINSTANCE *hModule){
+
+}
+
+///////////////Vessel initialization
+
+DLLCLBK VESSEL *ovcInit(OBJHANDLE hvessel, int flightmodel){
+    
+	return new UCFO(hvessel, flightmodel);
+
+}
+
+/////////////Vessel memory cleanup
+DLLCLBK void ovcExit(VESSEL *vessel){
+    
+	if(vessel) delete(UCFO*)vessel;
+	
+}
---
+++ src_Windows/main.h
@@ -0,0 +1,214 @@
+/////////////////////////////////////////////////////
+//          Copyright (c)2024 Matías Saibene       //
+//ORBITER MODULE: UNIVERSAL CARS FOR ORBITER (UCFO)//
+//          Licenced under the MIT Licence         //
+//              main.h  Main header file           //
+/////////////////////////////////////////////////////
+#pragma once
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#define STRICT 1
+
+#include <cmath>
+#include <cstddef>
+#include <exception>
+#include <sys/types.h>
+#include <cstring>
+#include <cstdio>
+#include <cstdint>
+#include <limits>
+
+#include "HEADERS//Orbitersdk.h"
+#include "HEADERS//OrbiterAPI.h"
+#include "HEADERS//ModuleAPI.h"
+#include "HEADERS//VesselAPI.h"
+#include "HEADERS//XRSound.h"
+#include "HEADERS//GraphicsAPI.h"
+
+const double UCFO_ISP = 2e4;
+
+const double UCFO_FUEL_MASS = 50; //Fuel mass in kg.
+
+const VECTOR3 ENGINE_LOCATION = {0, 0, 0};
+
+const VECTOR3 FORWARD_DIRECTION = {0, 0, 1};
+
+const VECTOR3 BACKWARD_DIRECTION = {0, 0, -1};
+
+static const int ntdvtx = 12;
+
+class UCFO : public VESSEL4{
+
+    public:
+
+        UCFO(OBJHANDLE hVessel, int flightmodel);
+
+        virtual ~UCFO();
+
+        void TerminateAtError(const char *error, const char * className, const char *type);
+
+        void clbkSetClassCaps(FILEHANDLE cfg) override;
+        void clbkLoadStateEx(FILEHANDLE scn, void *vs) override;
+        void clbkPostCreation() override;
+        void clbkPreStep(double simt, double simdt, double mjd) override;
+        void clbkPostStep(double simt, double simdt, double mjd) override;
+        int clbkConsumeBufferedKey(DWORD key, bool down, char *kstate)override;
+        int clbkConsumeDirectKey(char *kstate) override;
+
+        void EnginePower();
+        void Brakes();
+        void SetContactTouchdownPoints();
+
+
+        double UpdateLvlWheelsTrails();
+
+        VISHANDLE vhUCFO;
+        MESHHANDLE mhUCFO, mhcockpitUCFO;
+        unsigned int uimesh_UCFO;
+        unsigned int uimesh_Cockpit = 1;
+
+    private:
+
+        char MeshName[256];
+        double size;
+        double empty_mass;
+
+        int front_left_wheel_id;
+        int front_right_wheel_id;
+        int rear_right_wheel_id;
+        int rear_left_wheel_id;
+        int steering_wheel_id;
+        VECTOR3 front_left_wheel_pos;
+        VECTOR3 front_right_wheel_pos;
+        VECTOR3 rear_left_wheel_pos;
+        VECTOR3 rear_right_wheel_pos;
+        VECTOR3 left_headlight_pos;
+        VECTOR3 right_headlight_pos;
+        VECTOR3 left_tail_light_pos;
+        VECTOR3 right_tail_light_pos;
+        VECTOR3 left_backup_pos;
+        VECTOR3 right_backup_pos;
+        VECTOR3 camera_pos;
+        VECTOR3 TDP1;
+        VECTOR3 TDP2;
+        VECTOR3 TDP3;
+        VECTOR3 TDP4;
+        VECTOR3 TDP5;
+        VECTOR3 TDP6;
+        VECTOR3 TDP7;
+        VECTOR3 TDP8;
+
+        NOTEHANDLE hNote = NULL;
+        oapi::ScreenAnnotation *msg1_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg2_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg3_annotation =   (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg4_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg5_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg6_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg7_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg8_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg9_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg10_annotation = (oapi::ScreenAnnotation *) hNote;
+        oapi::ScreenAnnotation *msg11_annotation = (oapi::ScreenAnnotation *) hNote;
+        
+        const char *cockpit_meshname = NULL;
+
+        double lvlwheeltrails;
+
+        double anim_delay = 0.5;
+
+        unsigned int anim_steering_wheel;
+        
+
+        unsigned int anim_custom_1;
+        unsigned int anim_custom_2;
+        unsigned int anim_custom_3;
+
+        unsigned int anim_right_front_wheel_rotation, anim_left_front_wheel_rotation, anim_right_rear_wheel_rotation, anim_left_rear_wheel_rotation;
+
+        double right_rear_wheel_rotation, right_front_wheel_rotation, left_rear_wheel_rotation, left_front_wheel_rotation;
+
+        unsigned int anim_right_front_wheel_steer, anim_left_front_wheel_steer;
+        unsigned int anim_right_front_wheel_travel, anim_left_front_wheel_travel, anim_right_rear_wheel_travel,
+        anim_left_rear_wheel_travel;
+
+        THRUSTER_HANDLE th_dummy;
+        THGROUP_HANDLE thg_dummy;
+        BEACONLIGHTSPEC left_headlight_beacon_spec, right_headlight_beacon_spec, right_tail_light_spec, left_tail_light_spec, left_turning_wheel, right_turning_wheel, beacon, stop_light, left_backup_light_spec, right_backup_light_spec;
+        SURFHANDLE skin[1];
+        char skinpath[256];
+        LightEmitter *left_headlight, *right_headlight, *left_tail_light_point, *right_tail_light_point;
+        COLOUR4 col_white_d = {0.9,0.8,1,0};
+	    COLOUR4 col_white_s = {1.9,0.8,1,0};
+	    COLOUR4 col_white_a = {0,0,0,0};
+        COLOUR4 col_red_d = {1, 0, 0, 1};
+        COLOUR4 col_red_s = {1, 0, 0, 1};
+        COLOUR4 col_red_a = {1, 0, 0, 1};
+        VECTOR3 col_white = {1, 1, 1};
+        VECTOR3 col_red = {1.0, 0.0, 0.0};
+
+        int * sa;
+
+
+        double steering_angle;
+        double R;
+        double wheel_base;
+        double angle_right;
+        double angle_left;
+        double wheel_track;
+        double turn_radius;
+        char brake_status = 'F';
+        VECTOR3 vel;
+        double mu_dyn = 0.7;
+        double mu_sta = 1.0; //static friction coefficient (rubber on asphalt)
+        double max_weight;
+        double wheel_radius = 0.322;
+        double main_fuel_tank_max;
+        char drive_status;
+        double pitch;
+        double yaw, bank;
+        VECTOR3 omega;
+        double x, z;
+        VECTOR3 y;
+        VECTOR3 angvel;
+        int mass;
+        PROPELLANT_HANDLE Fuel;
+        double max_weight_rear, max_weight_front;
+        VECTOR3 rear_right_wheel_contact, rear_left_wheel_contact, front_right_wheel_contact, front_left_wheel_contact;
+        VECTOR3 max_weight_vector;
+        double front_stiffness, rear_stiffness, body_stiffness;
+        double front_damping, rear_damping, body_damping;
+        double travel = 0.06;
+        double front_left_tilt_angle,rear_right_tilt_angle, rear_left_tilt_angle;
+        double front_right_displacement, front_left_displacement, rear_right_displacement, rear_left_displacement;
+        double front_right_wheel_force, front_left_wheel_force, rear_right_wheel_force, rear_left_wheel_force;
+        VECTOR3 front_right_axle_axis, front_left_axle_axis, rear_right_axle_axis, rear_left_axle_axis;
+        VECTOR3 velFR, velFL, velRR, velRL;
+        VECTOR3 front_right_skid_force, front_left_skid_force, rear_right_skid_force, rear_left_skid_force;
+        VECTOR3 front_right_skid_force_axis, front_left_skid_force_axis, rear_right_skid_force_axis, rear_left_skid_force_axis;
+        VECTOR3 front_right_turning_force, front_left_turning_force, rear_right_turning_force, rear_left_turning_force;
+        VECTOR3 front_right_turning_force_axis, front_left_turning_force_axis, rear_right_turning_force_axis, rear_left_turning_force_axis;
+        VECTOR3 front_right_wheel_contact_local, front_left_wheel_contact_local, rear_right_wheel_contact_local, rear_left_wheel_contact_local;
+        char FR_status, FL_status, RR_status, RL_status;
+        char headlight_status;
+        VECTOR3 front_right_wheel_axle, front_left_wheel_axle, rear_right_wheel_axle, rear_left_wheel_axle;
+
+        //Displacements for lateral static force model
+
+        double dxFR;
+        double dxFL;
+        double dxRR;
+        double dxRL;
+
+        double dzFR;
+        double dzFL;
+        double dzRR;
+        double dzRL;
+
+};
+
+#endif //!__MAIN_H
